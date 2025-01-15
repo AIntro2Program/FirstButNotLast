3ac6ebddb6e80526edf6bee1355b13917aad37be
AIntro2Program <a.sobirov@newuu.uz>
2025-01-15 10:20:09 +0500

    My First Change

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+    int age;
+    string name;
+    cout << "Enter your name: ";
+    cin >> name;
+    cout << "Please enter your age: ";
+    cin >> age;
+    cout << "Your name is " << name << " and your age is " << age << endl;
+    return 0;
+}
